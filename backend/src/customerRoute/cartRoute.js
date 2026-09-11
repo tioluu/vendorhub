b2a0b30108e3cfe9163833c00c6b25aefd91bb67
tolu <babalola.it@gmail.com>
2026-09-11 19:43:41 +0100

    fix(cart): remove authentication middleware from viewCart route

--- backend/src/customerRoute/cartRoute.js
+++ backend/src/customerRoute/cartRoute.js
@@ -1,9 +1,8 @@
 import express, { Router } from "express";
 import { viewCart } from "../customerController/cartController.js";
-import { authenticateToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
  
-router.get ("/view-cart", authenticateToken, viewCart);
+router.get ("/view-cart", viewCart);
 
 export default router;
