484a00e7b2ef17dd623bedb7de68970ab6c39dbf
tolu <babalola.it@gmail.com>
2026-09-11 23:52:15 +0100

    fix(server): use environment variable for server port configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,7 +21,7 @@ app.use("/api/categories", categoryRoutes);
 app.use("/api/cart", cartRoutes);
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
